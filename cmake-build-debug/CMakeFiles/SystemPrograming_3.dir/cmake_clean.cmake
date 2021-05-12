6a694cc664f5c628c1045e803f0b1f0919d171aa
kfire <kfirettinger@gmail.com>
2021-05-12 19:02:01 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/SystemPrograming_3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SystemPrograming_3.dir/counter.c.o"
+  "CMakeFiles/SystemPrograming_3.dir/primes.c.o"
+  "SystemPrograming_3"
+  "SystemPrograming_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SystemPrograming_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
